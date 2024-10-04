c3bdb22e8350d1c3b5a326307ba684100dc8ffb1
indahkasturi <indahkasturiii@gmail.com>
2024-10-04 10:39:11 +0700

    gemini backEnd

--- Server/controllers/GeminiConroller.js
+++ Server/controllers/GeminiConroller.js
@@ -1,37 +1,31 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI('AIzaSyAYsy61DInnskz7W5djhX3gaCiSG1LshsU');
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+class GeminiController {
+  static async gemini(req, res, next) {
+    try {
+      const {prompt} = req.body;
 
-class GeminiController{
-    static async gemini (req, res, next){
-try {
-    const prompt = req.body
-    
-    const result = await model.generateContent(prompt);
-    const response = await result.response.text
-    let text = response.text()
+      const result = await model.generateContent(prompt)
 
-    
-    res.send(text)
-} catch (error) {
-    next(error)
+      res.send(result.response.text());
+    } catch (error) {
+      next(error);
+    }
+  }
 }
-}
-}
-module.exports = GeminiController
+module.exports = GeminiController;
 // const gemini = require("../helper/GeminiAi");
 
-
 // const API_KEY = 'AIzaSyAYsy61DInnskz7W5djhX3gaCiSG1LshsU';
-
 
 // module.exports = class GeminiController {
 // static async generateContent(req, res, next) {
 //   try {
 //       const {album} = req.body
 //       let data = await gemini(album)
-   
+
 //     res.status(200).json(data);
 
 //   } catch (error) {
