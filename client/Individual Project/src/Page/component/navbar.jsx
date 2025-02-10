619fdf6c62e28907c0cb20ea15aa542f5319c8ad
indahkasturi <indahkasturiii@gmail.com>
2025-02-10 14:00:48 +0700

    resolve conflic

--- client/Individual Project/src/Page/component/navbar.jsx
+++ client/Individual Project/src/Page/component/navbar.jsx
@@ -1,9 +1,22 @@
 import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
-
 export default function Navbar() {
   const navigate = useNavigate();
+
+  const handleAdminClick = () => {
+    const role = localStorage.getItem("role");
+    if (role !== "admin") {
+      Swal.fire({
+        icon: "error",
+        title: "Access Denied",
+        text: "You do not have permission to access this page.",
+      });
+    } else {
+      navigate("/admin");
+    }
+  };
+
   const logout = async () => {
     try {
       localStorage.clear();
@@ -16,19 +29,22 @@ export default function Navbar() {
       });
     }
   };
+
   return (
     <section>
       <nav className="navbar navbar-light bg-light">
-      <button className="btn btn-outline-primary"><Link to={`/admin`}> Admin Only </Link> </button>
-        <div >
-        <button className="btn btn-outline-primary"><Link to={`/`}>Home</Link></button>
-        <button className="btn btn-outline-primary"><Link to={`/cart`}> Cart </Link> </button>
+        <button className="btn btn-outline-primary" onClick={handleAdminClick}>
+          Admin Only
+        </button>
+        <div>
+          <button className="btn btn-outline-primary">
+            <Link to={`/`}>Home</Link>
+          </button>
+          <button className="btn btn-outline-primary">
+            <Link to={`/cart`}>Cart</Link>
+          </button>
         </div>
-        
-        <button
-          onClick={logout}
-          className="btn btn-outline-danger"
-        >
+        <button onClick={logout} className="btn btn-outline-danger">
           Logout
         </button>
       </nav>
